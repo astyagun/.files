23ea3704e468d96ecfddd0ca639897f7e28c2640
Anton Styagun <astyagun@gmail.com>
2019-06-09 16:44:11 +0300

    Install Zsh prompt and enable LS_COLORS settings

--- zsh/plugins.zsh
+++ zsh/plugins.zsh
@@ -1,18 +1,18 @@
 function zplg_prezto_module() {
-  zplg ice wait'0' svn silent
+  zplg ice wait'0' svn lucid
   zplg snippet PZT::modules/$1
 }
 
 # Zsh setup
 
-zplg ice svn silent
+zplg ice svn lucid
 zplg snippet PZT::modules/history
 
 HISTORY_KEYS='bindkey -M emacs "^P" history-substring-search-up; bindkey -M emacs "^N" history-substring-search-down'
-zplg ice wait'0' atload"$HISTORY_KEYS" silent
+zplg ice wait'0' atload"$HISTORY_KEYS" lucid
 zplg load zsh-users/zsh-history-substring-search
 
-zplg ice wait'0' silent
+zplg ice wait'0' lucid
 zplg load zsh-users/zsh-autosuggestions
 
 zplg_prezto_module completion
@@ -20,11 +20,17 @@ zplg_prezto_module completion
 
 # Functions, aliases and completions
 
+zstyle ':prezto:module:utility:ls' color 'yes'
+zstyle ':prezto:module:utility:diff' color 'yes'
+zstyle ':prezto:module:utility:wdiff' color 'yes'
+zstyle ':prezto:module:utility:make' color 'yes'
+zstyle ':prezto:module:utility' correct 'yes'
+zstyle ':prezto:module:utility' safe-ops 'yes'
 zplg_prezto_module utility
 zplg_prezto_module git
 zplg_prezto_module homebrew
 
-zplg ice wait'0' silent
+zplg ice wait'0' lucid
 zplg load akarzim/zsh-docker-aliases
 
 
@@ -32,11 +38,20 @@ zplg load akarzim/zsh-docker-aliases
 
 alias cdc='code cd'
 alias mydiraction_dispatch=_diraction-dispatch
-zplg ice wait'0' cp'__diraction-dispatch -> _mydiraction-dispatch' atload'diraction create code ~/Code' silent
+zplg ice wait'0' cp'__diraction-dispatch -> _mydiraction-dispatch' atload'diraction create code ~/Code' lucid
 zplg load AdrieanKhisbe/diractions
 
 
+# Visual
+
+zplugin ice pick'async.zsh' src'pure.zsh'
+zplugin light sindresorhus/pure
+PROMPT=$(echo "$PROMPT" | sed 's/magenta/green/')
+
+zplugin ice wait'0' atclone'gdircolors -b LS_COLORS > clrs.zsh' atpull'%atclone' pick'clrs.zsh' lucid
+zplugin load trapd00r/LS_COLORS
+
 # Syntax highlighting, to be loaded last, also atinit'' executes callbacks for completions
-zplg ice wait'0c' atinit'zpcompinit; zpcdreplay' silent
+zplg ice wait'0c' atinit'zpcompinit; zpcdreplay' lucid
 export ZSH_HIGHLIGHT_HIGHLIGHTERS=(main brackets cursor line pattern root)
 zplg load zsh-users/zsh-syntax-highlighting
