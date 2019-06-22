e93bde76f4adbef24c5bbd1bc43c39b2874aee5c
Anton Styagun <astyagun@gmail.com>
2019-06-22 11:47:29 +0300

    Add more directories to $PATH

--- zsh/env.zsh
+++ zsh/env.zsh
@@ -6,4 +6,6 @@ export LESS='-F -g -i -M -R -w -X -z-4'
 export PATH="$HOME/.gem/ruby/2.3.0/bin:$PATH"
 export PATH="$HOME/Library/Python/3.7/bin:$PATH"
 export PATH="/usr/local/heroku/bin:$PATH"
-export PATH=$PATH:$HOME/bin
+export PATH="$HOME/.files/bin:$PATH"
+export PATH="$HOME/Code/automation:$PATH"
+export PATH="$HOME/bin:$PATH"
