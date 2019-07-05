00bac9cd77fd685381f4683eda8c0c28507c81e6
Anton Styagun <astyagun@gmail.com>
2019-07-05 22:30:20 +0300

    Add /usr/local/sbin to $PATH

--- zsh/env.zsh
+++ zsh/env.zsh
@@ -3,6 +3,7 @@ export VISUAL='mvim -f'
 
 export LESS='-F -g -i -M -R -w -X -z-4'
 
+export PATH="/usr/local/sbin:$PATH"
 export PATH="$HOME/.gem/ruby/2.3.0/bin:$PATH"
 export PATH="$HOME/Library/Python/3.7/bin:$PATH"
 export PATH="/usr/local/heroku/bin:$PATH"
