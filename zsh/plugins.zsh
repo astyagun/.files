b96ae56e27feda182007939b85c718639c86dfda
Anton Styagun <astyagun@ya.ru>
2020-03-18 12:57:57 +0300

    React to renaming of Zplugin into Zinit

--- zsh/plugins.zsh
+++ zsh/plugins.zsh
@@ -1,24 +1,24 @@
-function zpl_prezto_module() {
-  zpl ice wait'0' svn lucid
-  zpl snippet PZT::modules/$1
+function zinit_prezto_module() {
+  zinit ice wait'0' svn lucid
+  zinit snippet PZT::modules/$1
 }
 
-function zpl_omz_completion_plugin() {
-  zpl ice wait'0' svn pick'' lucid
-  zpl snippet OMZ::plugins/$1
+function zinit_omz_completion_plugin() {
+  zinit ice wait'0' svn pick'' lucid
+  zinit snippet OMZ::plugins/$1
 }
 
 # Zsh setup
 
-zpl ice svn lucid
-zpl snippet PZT::modules/history
+zinit ice svn lucid
+zinit snippet PZT::modules/history
 
 HISTORY_KEYS='bindkey -M emacs "^P" history-substring-search-up; bindkey -M emacs "^N" history-substring-search-down'
-zpl ice wait'0' atload"$HISTORY_KEYS" lucid
-zpl load zsh-users/zsh-history-substring-search
+zinit ice wait'0' atload"$HISTORY_KEYS" lucid
+zinit load zsh-users/zsh-history-substring-search
 
-zpl_prezto_module completion
-zpl_prezto_module directory
+zinit_prezto_module completion
+zinit_prezto_module directory
 
 
 # Functions, aliases and completions
@@ -29,42 +29,42 @@ zstyle ':prezto:module:utility:wdiff' color 'yes'
 zstyle ':prezto:module:utility:make' color 'yes'
 zstyle ':prezto:module:utility' correct 'yes'
 zstyle ':prezto:module:utility' safe-ops 'yes'
-zpl_prezto_module utility
+zinit_prezto_module utility
 
-zpl_prezto_module git
-zpl_prezto_module homebrew
+zinit_prezto_module git
+zinit_prezto_module homebrew
 
-zpl ice wait'0' lucid
-zpl load akarzim/zsh-docker-aliases
+zinit ice wait'0' lucid
+zinit load akarzim/zsh-docker-aliases
 
-zpl ice wait'0' lucid
-zpl load djui/alias-tips
+zinit ice wait'0' lucid
+zinit load djui/alias-tips
 
-zpl_omz_completion_plugin httpie
+zinit_omz_completion_plugin httpie
 
 
 # Navigation
 
 alias cdc='code cd'
 alias mydiraction_dispatch=_diraction-dispatch
-zpl ice wait'0' cp'__diraction-dispatch -> _mydiraction-dispatch' atload'diraction create code ~/Code' lucid
-zpl load AdrieanKhisbe/diractions
+zinit ice wait'0' cp'__diraction-dispatch -> _mydiraction-dispatch' atload'diraction create code ~/Code' lucid
+zinit load AdrieanKhisbe/diractions
 
 
 # Visual
 
 zstyle :prompt:pure:prompt:success color green
-zpl ice pick'async.zsh' src'pure.zsh'
-zpl light sindresorhus/pure
+zinit ice pick'async.zsh' src'pure.zsh'
+zinit light sindresorhus/pure
 
-zpl ice wait'0' lucid
-zpl load ael-code/zsh-colored-man-pages
+zinit ice wait'0' lucid
+zinit load ael-code/zsh-colored-man-pages
 
 # Syntax highlighting, to be loaded last, also atinit'' executes callbacks for completions
-zpl ice wait'0' atinit'zpcompinit; zpcdreplay' lucid
+zinit ice wait'0' atinit'zpcompinit; zpcdreplay' lucid
 export ZSH_HIGHLIGHT_HIGHLIGHTERS=(main brackets cursor line pattern root)
-zpl load zsh-users/zsh-syntax-highlighting
+zinit load zsh-users/zsh-syntax-highlighting
 
 # Needs to be loaded after zsh-syntax-highlighting
-zpl ice wait'0' atload'_zsh_autosuggest_start' lucid
-zpl load zsh-users/zsh-autosuggestions
+zinit ice wait'0' atload'_zsh_autosuggest_start' lucid
+zinit load zsh-users/zsh-autosuggestions
