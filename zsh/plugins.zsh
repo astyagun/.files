03dc973478f1adaf06bd65dedf83971a91b93743
Anton Styagun <astyagun@ya.ru>
2020-03-18 13:15:24 +0300

    Load Zsh plugins faster
    By using Zinit `light` command instead of `load` command

--- zsh/plugins.zsh
+++ zsh/plugins.zsh
@@ -15,7 +15,7 @@ zinit snippet PZT::modules/history
 
 HISTORY_KEYS='bindkey -M emacs "^P" history-substring-search-up; bindkey -M emacs "^N" history-substring-search-down'
 zinit ice wait'0' atload"$HISTORY_KEYS" lucid
-zinit load zsh-users/zsh-history-substring-search
+zinit light zsh-users/zsh-history-substring-search
 
 zinit_prezto_module completion
 zinit_prezto_module directory
@@ -35,10 +35,10 @@ zinit_prezto_module git
 zinit_prezto_module homebrew
 
 zinit ice wait'0' lucid
-zinit load akarzim/zsh-docker-aliases
+zinit light akarzim/zsh-docker-aliases
 
 zinit ice wait'0' lucid
-zinit load djui/alias-tips
+zinit light djui/alias-tips
 
 zinit_omz_completion_plugin httpie
 
@@ -48,7 +48,7 @@ zinit_omz_completion_plugin httpie
 alias cdc='code cd'
 alias mydiraction_dispatch=_diraction-dispatch
 zinit ice wait'0' cp'__diraction-dispatch -> _mydiraction-dispatch' atload'diraction create code ~/Code' lucid
-zinit load AdrieanKhisbe/diractions
+zinit light AdrieanKhisbe/diractions
 
 
 # Visual
@@ -58,13 +58,13 @@ zinit ice pick'async.zsh' src'pure.zsh'
 zinit light sindresorhus/pure
 
 zinit ice wait'0' lucid
-zinit load ael-code/zsh-colored-man-pages
+zinit light ael-code/zsh-colored-man-pages
 
 # Syntax highlighting, to be loaded last, also atinit'' executes callbacks for completions
 zinit ice wait'0' atinit'zpcompinit; zpcdreplay' lucid
 export ZSH_HIGHLIGHT_HIGHLIGHTERS=(main brackets cursor line pattern root)
-zinit load zsh-users/zsh-syntax-highlighting
+zinit light zsh-users/zsh-syntax-highlighting
 
 # Needs to be loaded after zsh-syntax-highlighting
 zinit ice wait'0' atload'_zsh_autosuggest_start' lucid
-zinit load zsh-users/zsh-autosuggestions
+zinit light zsh-users/zsh-autosuggestions
