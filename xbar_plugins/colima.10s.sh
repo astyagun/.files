25b3d4ab512d3d8efc7bf79ef54edbd2e7abdce2
Anton Styagun <astyagun@ya.ru>
2022-04-15 00:24:06 +0300

    Refactor Colima xbar plugin

--- xbar_plugins/colima.10s.sh
+++ xbar_plugins/colima.10s.sh
@@ -2,26 +2,17 @@
 
 # Simple xbar plugin to control Colima VM
 
-ICON_RUNNING=C
-ICON_STOPPED='c | color=gray'
 THIS_FILE="$0"
+
+ICON_RUNNING='C'
+ICON_STOPPED='c | color=gray'
+START_ACTION="Start | shell='$THIS_FILE' param1=start"
+STOP_ACTION="Stop | shell='$THIS_FILE' param1=stop"
 
 eval "$(/opt/homebrew/bin/brew shellenv)"
 
 function colima_is_running() {
-  if [[ -f ~/.lima/colima/qemu.pid ]]; then
-    return 0
-  else
-    return 1
-  fi
-}
-
-function action() {
-  if colima_is_running; then
-    echo "Stop | shell='$THIS_FILE' param1=stop"
-  else
-    echo "Start | shell='$THIS_FILE' param1=start"
-  fi
+  [[ -f ~/.lima/colima/qemu.pid ]] && return 0 || return 1
 }
 
 if [[ "$1" == 'start' ]]; then
@@ -36,4 +27,4 @@ fi
 
 echo "$(colima_is_running && echo "$ICON_RUNNING" || echo "$ICON_STOPPED")"
 echo '---'
-echo "$(action)"
+echo "$(colima_is_running && echo "$STOP_ACTION" || echo "$START_ACTION")"
