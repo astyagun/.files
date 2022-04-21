851c94e408dff373c9e0df0b2fa944699215252c
Anton Styagun <astyagun@ya.ru>
2022-04-21 12:56:25 +0300

    Remap fzf keys after calling fzf init script

--- zsh/fzf.zsh
+++ zsh/fzf.zsh
@@ -1,6 +1,6 @@
+# See `brew info fzf` for installation instructions
+[ -f ~/.fzf.zsh ] && source ~/.fzf.zsh
+
 # Remap Ctrl+T from fzf back to original
 bindkey -M emacs '^X^F' fzf-file-widget
 bindkey -M emacs '^T' transpose-chars
-
-# See `brew info fzf` for installation instructions
-[ -f ~/.fzf.zsh ] && source ~/.fzf.zsh
