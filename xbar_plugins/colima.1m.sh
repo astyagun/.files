88c48e409517ad00d7bd0a0ed6159f96abe3f42c
Anton Styagun <astyagun@ya.ru>
2022-04-23 13:15:39 +0300

    Simplify Colima xbar plugin

--- xbar_plugins/colima.1m.sh
+++ xbar_plugins/colima.1m.sh
@@ -1,31 +1,28 @@
 #!/usr/bin/env bash
 
-# Simple xbar plugin to control Colima VM
+# Simple xbar plugin to monitor Colima VM status and stop the VM
 #
-#  <xbar.title>Colima</xbar.title>
+#  <xbar.title>Colima monitor</xbar.title>
 #  <xbar.version>v1.0</xbar.version>
 #  <xbar.author>Anton Styagun</xbar.author>
 #  <xbar.author.github>astyagun</xbar.author.github>
-#  <xbar.desc>Show status and allow to start/stop Colima VM</xbar.desc>
+#  <xbar.desc>Show status and allow to stop Colima VM</xbar.desc>
 #  <xbar.dependencies>Colima, Lima</xbar.dependencies>
+
+# Main purpose is to notice if I've forgotten to turn the VM off and be able to do so. Because of that refresh interval
+# can be longer, like 1m, and there's no need to detect state of VM "starting or stopping".
 
 THIS_FILE="$0"
 
-ICON_INBETWEEN='… | color=gray'
 ICON_RUNNING='C'
 ICON_STOPPED='c | color=gray'
-START_ACTION="Start | shell='$THIS_FILE' param1=start"
+START_ACTION='Stopped'
 STOP_ACTION="Stop | shell='$THIS_FILE' param1=stop"
 
 if [[ -f /opt/homebrew/bin/brew ]]; then
   eval "$(/opt/homebrew/bin/brew shellenv)"
 else
   eval "$(/usr/local/bin/brew shellenv)"
-fi
-
-if [[ "$1" == 'start' ]]; then
-  colima start && LIMA_INSTANCE=colima lima sudo sysctl -p
-  exit
 fi
 
 if [[ "$1" == 'stop' ]]; then
@@ -35,10 +32,6 @@ fi
 
 if [[ -S ~/.lima/colima/ssh.sock ]]; then
   echo "$ICON_RUNNING"
-  echo '---'
-  echo "$STOP_ACTION"
-elif [[ -f ~/.lima/colima/qemu.pid ]]; then
-  echo "$ICON_INBETWEEN"
   echo '---'
   echo "$STOP_ACTION"
 else
