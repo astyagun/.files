43328c02a2db411eb97f7812a021486e39338072
Anton Styagun <astyagun@ya.ru>
2024-02-29 14:28:55 +0300

    Update Colima status monitor for new version of Colima

--- xbar_plugins/colima.1m.sh
+++ xbar_plugins/colima.1m.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-# Simple xbar plugin to monitor Colima VM status and stop the VM
+# Simple xbar plugin to monitor Colima VM status
 #
 # <xbar.title>Colima monitor</xbar.title>
 # <xbar.version>v1.0</xbar.version>
@@ -17,12 +17,8 @@
 # Main purpose is to notice if I've forgotten to turn the VM off and be able to do so. Because of that refresh interval
 # can be longer, like 1m, and there's no need to detect state of VM "starting or stopping".
 
-THIS_FILE="$0"
-
 ICON_RUNNING='C'
 ICON_STOPPED='c'
-START_ACTION='Stopped'
-STOP_ACTION="Stop | shell='$THIS_FILE' param1=stop"
 
 if [[ -f /opt/homebrew/bin/brew ]]; then
   eval "$(/opt/homebrew/bin/brew shellenv)"
@@ -30,19 +26,10 @@ else
   eval "$(/usr/local/bin/brew shellenv)"
 fi
 
-if [[ "$1" == 'stop' ]]; then
-  colima stop
-  exit
-fi
-
-if [[ -S ~/.lima/colima/ssh.sock ]]; then
+if [[ -S ~/.colima/_lima/colima/ssh.sock ]]; then
   echo "$ICON_RUNNING"
-  echo '---'
-  echo 'Refresh | refresh=true'
-  echo "$STOP_ACTION"
 else
   echo "$ICON_STOPPED"
-  echo '---'
-  echo 'Refresh | refresh=true'
-  echo "$START_ACTION"
 fi
+echo '---'
+echo 'Refresh | refresh=true'
