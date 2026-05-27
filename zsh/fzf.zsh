8d2d895f9ded157741ba4455c384c6bc197fccb8
Anton Styagun <astyagun@ya.ru>
2026-05-27 14:12:35 +0300

    Fix fzf + Zsh integration

--- zsh/fzf.zsh
+++ zsh/fzf.zsh
@@ -1,5 +1,4 @@
-# See `brew info fzf` for installation instructions
-[ -f ~/.fzf.zsh ] && source ~/.fzf.zsh
+source <(fzf --zsh)
 
 # Remap Ctrl+T from fzf back to original
 bindkey -M emacs '^X^F' fzf-file-widget
